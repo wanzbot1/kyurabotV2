1ac0abab3839713020cc4db1796c1728915dec1d
Kyura <95233385+KyuraSaja@users.noreply.github.com>
2021-12-01 09:39:50 +0800

    apdet beli bot dot je es

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -1,20 +1,20 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
-	conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╠═〘 Beli Bot 〙 ═
-╠➥ *1 Bulan* :      *Rp 15000*
-╠➥ *Permanen* : *Rp 25000*
-╠➥ *Premium* :   *Rp 10000*
-╠➥ *Sc Bot* :        *Masih Beta*
-║
-╠═〘 PEMBAYARAN 〙 ═
-╠➥ Gopay, Dana, Dan Pulsa
-║
-╠═ Tertarik Untuk Beli Bot Ini?
-╠➥Ketuk Tombol Di Bawah Ya
-║
-╠═ ©2021 Rpg wabot-aq
-╠═ Scrip original by Nurutomo
-╠═〘 ${namabot} 〙 ═`.trim(), footer, 'Dana', '#viadana', 'OVO', '#viaovo', 'Pulsa', '#viapulsa', m)
+	conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╭─˗ˏˋ *Beli Bot* ´ˎ˗
+│ ✎ *1 Bulan* :      *Rp 15000*
+│ ✎ *Permanen* : *Rp 25000*
+│ ✎ *Premium* :   *Rp 10000*
+│ ✎ *Sc Bot* :        *Masih Beta*
+│ 
+╭─˗ˏˋ *Pembayaran* ´ˎ˗
+│ ✎ Gopay, Dana, Dan Pulsa
+│ 
+│ ✎ Tertarik Untuk Beli Bot Ini?
+│ ✎ Ketuk Tombol Di Bawah Ya
+│
+│ ✎ ©2021 KyuraBot
+│ ✎ Script original by Nurutomo
+╰‿‿‿‿‿‿‿‿〘 ${namabot} 〙 ═`.trim(), footer, 'Dana', '#viadana', 'OVO', '#viaovo', 'Pulsa', '#viapulsa', m)
 }
 
 handler.command = /^sewa(bot)?$/i
