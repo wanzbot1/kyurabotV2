cbb99d1c1851c4374490aeccbe3bc13e4b4e8bec
Kyura <95233385+KyuraSaja@users.noreply.github.com>
2021-12-01 09:58:59 +0800

    Update viadana.js

--- plugins/viadana.js
+++ plugins/viadana.js
@@ -1,30 +1,30 @@
 let handler = async (m, { conn }) => {
-	conn.reply(m.chat, `╠═〘 PEMBAYARAN 〙 ═
-╠═ Untuk Pembayaran Via DANA
-╠➥ Ke nomer ini
-║➥ 089677763976
-║
-╠═〘 Fitur 〙 ═
-║
-╠➥ *FITUR DOWNLOADER 500++
-║     YouTube, Twitter, 
-║     Tiktok, Instagram, dll.
-║    *FITUR STIKER*
-╠➥ *FITUR ADMIN*
-║    *KICK ORANG*
-║    *JADIIN ADMIN*
-╠➥ *FITUR ISLAM*
-║   *QURAN*
-║   *CEK JADWAL SHALAT*
-╠➥ *INTERNET*
-║   *BRAINLY*
-║   *GOOGLE*
-╠➥ *DAN 200 LEBIH*
-║   *FITUR LAINNYA*
-║ 
-╠═ ©2021 Rpg wabot-aq
-╠═ Scrip original by Nurutomo
-╠═〘 ${namabot} 〙 ═`.trim(), m)
+	conn.reply(m.chat, `╭─˗ˏˋ *Pembayaran* ´ˎ˗
+│ ✎ Untuk Pembayaran Via DANA
+│ ✎ Ke Nomor = 081998903280
+│ ✎ Bila Perlu hubungi owner 
+│ 
+╭─˗ˏˋ *Fitur Bot* ´ˎ˗
+│
+│ ✎ *FITUR DOWNLOADER 500++
+│    YouTube, Twitter, 
+│     Tiktok, Instagram, dll.
+│    *FITUR STIKER*
+│ ✎ *FITUR ADMIN*
+│    *KICK ORANG*
+│    *JADIIN ADMIN*
+│ ✎ *FITUR ISLAM*
+│   *QURAN*
+│   *CEK JADWAL SHALAT*
+│ ✎ *INTERNET*
+│   *BRAINLY*
+│   *GOOGLE*
+│ ✎ *DAN 200 LEBIH*
+│   *FITUR LAINNYA*
+│ 
+│ ✎ ©2021 Kyura Bot
+│ ✎ Script original by Nurutomo
+╰‿‿˗ˏˋ *${namabot}* ´ˎ˗〘 ${namabot} 〙 ═`.trim(), m)
 }
 
 handler.command = /^viadana$/i
