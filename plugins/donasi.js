8c15577b0a9c75b88066d100022283269778791a
Kyura <95233385+KyuraSaja@users.noreply.github.com>
2021-12-01 10:02:21 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
-┌〔 Donasi • Emoney - Pulsa〕
-├ PULSA : 081998903280
-├ Dana/Ovo/Gopay : 081998903280
-Saweria : bit ly/donasisawer
-└────
-
-ingin donasi? Chat Owner
+╭─˗ˏˋ*Donasi • Emoney - Pulsa* ´ˎ˗
+│ ✎ PULSA : 081998903280
+│ ✎ Dana/Ovo/Gopay : 081998903280
+│ ✎ Bila Perlu hubungi owner 
+╰‿‿‿‿‿‿‿‿‿‿‿‿‿‿
 `.trim(), footer, 'Owner', '.owner')
 handler.help = ['donasi']
 handler.tags = ['info']
