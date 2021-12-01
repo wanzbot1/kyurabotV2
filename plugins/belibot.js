0e13908415741779b10c5e5c627f80e7f52fc0d6
Kyura <95233385+KyuraSaja@users.noreply.github.com>
2021-12-01 09:40:59 +0800

    Update belibot.js

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -14,7 +14,7 @@ let handler = async (m, { conn }) => {
 │
 │ ✎ ©2021 KyuraBot
 │ ✎ Script original by Nurutomo
-╰‿‿‿‿‿‿‿‿〘 ${namabot} 〙 ═`.trim(), footer, 'Dana', '#viadana', 'OVO', '#viaovo', 'Pulsa', '#viapulsa', m)
+╰‿‿˗ˏˋ *${namabot}* ´ˎ˗ `.trim(), footer, 'Dana', '#viadana', 'OVO', '#viaovo', 'Pulsa', '#viapulsa', m)
 }
 
 handler.command = /^sewa(bot)?$/i
