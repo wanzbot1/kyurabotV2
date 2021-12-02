f48d57e83a93e9439d46c3e2b02968e87704ae7e
Kyura <95233385+KyuraSaja@users.noreply.github.com>
2021-12-02 11:22:46 +0800

    Dahla errorrrrr

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -3,19 +3,16 @@ let handler = m => m
 
 handler.before = async (m) => {
     let chat = global.db.data.chats[m.chat]
-    if (chat.simi && !chat.isBanned) {
+    if (chat.simi && !chat.isBanned && !m.isGroup) {
         if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
         if (!m.text) return
-        
-
-let res = await fetch(global.API('bg', '/simi', { pesan: text })) 
-
-   let json = await res.json() 
-
-   if (json.status !== true) throw json 
-
-   m.reply(json.result.jawab) 
-
- }
+        ​let​ ​res​ ​=​ ​await​ ​fetch​(​global​.​API​(​'bg'​,​ ​'/simi'​,​ ​{​ ​pesan​: ​text​ ​}​)​)
+        if (!res.ok) return m.reply(eror)
+        let json = await res.json()
+        if (json.result == 'Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.') await m.reply('siminya blom diajarin, ajarin di https://simsimi.com/teach')
+        else await m.reply(`*Simi:* ${json.result.jawab}`)
+        return !0
+    }
+    return true
 }
 module.exports = handler
