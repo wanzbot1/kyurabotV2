cf322f6954f275307f259fb03657d999172177e0
Kyura <95233385+KyuraSaja@users.noreply.github.com>
2021-12-02 11:10:33 +0800

    Dahlah error mulu

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -6,13 +6,16 @@ handler.before = async (m) => {
     if (chat.simi && !chat.isBanned) {
         if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
         if (!m.text) return
-        let res = await fetch(global.API('bg', '/simi', { pesan: text }))
-        if (!res.ok) return m.reply(eror)
-        let json = await res.json()
-        if (json.result.jawab == 'Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.') await m.reply('siminya blom diajarin, ajarin di https://simsimi.com/teach')
-        else await m.reply(`*Simi:* ${json.result.jawab}`)
-        return !0
-    }
-    return true
+        
+
+let res = await fetch(global.API('bg', '/simi', { pesan: text })) 
+
+   let json = await res.json() 
+
+   if (json.status !== true) throw json 
+
+   m.reply(json.result.jawab) 
+
+ }
 }
 module.exports = handler
