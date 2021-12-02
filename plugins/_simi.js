f3bbc74df6c6db364afd1e68ba5caa9e5fd42fd9
Kyura <95233385+KyuraSaja@users.noreply.github.com>
2021-12-02 08:37:21 +0800

    Update _simi.js

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -3,10 +3,10 @@ let handler = m => m
 
 handler.before = async (m) => {
     let chat = global.db.data.chats[m.chat]
-    if (chat.simi && !chat.isBanned && !m.isGroup) {
+    if (chat.simi && !chat.isBanned) {
         if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
         if (!m.text) return
-        let res = await fetch(global.API('pencarikode', '/api/simsimii', { text: encodeURIComponent(m.text) }, 'apikey'))
+        let res = await fetch(global.API('bg', '/simi', { pesan: text }))
         if (!res.ok) return m.reply(eror)
         let json = await res.json()
         if (json.result == 'Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.') await m.reply('siminya blom diajarin, ajarin di https://simsimi.com/teach')
