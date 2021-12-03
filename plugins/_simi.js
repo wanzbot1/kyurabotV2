cf5648887cdc357b3a1dc0349a5cd70e0a824a6b
Kyura <95233385+KyuraSaja@users.noreply.github.com>
2021-12-03 12:11:51 +0800

    apdet API nyaa:0000000

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -6,11 +6,12 @@ handler.before = async (m) => {
     if (chat.simi && !chat.isBanned && !m.isGroup) {
         if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
         if (!m.text) return
-        ​let​ ​res​ ​=​ ​await​ ​fetch​(​global​.​API​(​'bg'​,​ ​'/simi'​,​ ​{​ ​pesan​: ​text​ ​}​)​)
+        let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(m.text), lc: "id" }, ''))
+        //let res = await fetch(global.API('rey', '/api/fun/simisimi-ind2?', { text: encodeURIComponent(m.text) }, 'apikey'))
         if (!res.ok) return m.reply(eror)
         let json = await res.json()
-        if (json.result == 'Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.') await m.reply('siminya blom diajarin, ajarin di https://simsimi.com/teach')
-        else await m.reply(`*Simi:* ${json.result.jawab}`)
+        if (json.success == 'maksdnya kak?') await m.reply('hah?\naku gk paham')
+        else await m.reply(`${json.success}`)
         return !0
     }
     return true
