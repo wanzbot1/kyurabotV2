919fb299194216d6acd3f49948e37906c4bef38e
wanzbot1 <96588684+wanzbot1@users.noreply.github.com>
2022-01-04 01:52:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -14,11 +14,11 @@
 gc1 = 'bit.ly/officialgrupkanaobot'
 gc2 = ''
 gc3 = ''
-global.linkGC = ['bit.ly/officialgrupkyurabot'] // ganti jadi group lu
-global.owner = ['6281998903280'] // Put your number here //owner eval
-global.kontak = ['6281998903280'] //Ketika ada yang ngetik #owner
-global.mods = ['6281998903280'] // Want some help?
-global.prems = ['6281998903280'] // Premium user has unlimited limit
+global.linkGC = [''] // ganti jadi group lu
+global.owner = ['601135268529'] // Put your number here //owner eval
+global.kontak = ['601135268529'] //Ketika ada yang ngetik #owner
+global.mods = ['601135268529'] // Want some help?
+global.prems = ['601135268529'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com', 
@@ -55,13 +55,13 @@ namakontak2 = 'Ardhi'
 
 // Sticker WM
 global.packname = 'From' // ganti aja
-global.author = 'KyuraBOT' // ganti aja
+global.author = 'icebotz' // ganti aja
 
 //yyy
 bc = 'KanaoBOT' //RFK Broadcast
-footer = '\n©Kyura'
-namabot = 'KanaoBOT'
-namalu = 'Kyura'
+footer = '\n© ICEBOT'
+namabot = ''
+namalu = ''
 
 
 // 
